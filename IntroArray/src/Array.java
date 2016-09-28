30016420164fcbc7efbc34b041bd156747406402
alejandromijares <alejandromijares@148.201.184.46>
2016-09-28 12:43:46 -0500

    Add 0

--- IntroArray/src/Array.java
+++ IntroArray/src/Array.java
@@ -4,7 +4,7 @@ public class Array {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int array[] = {1,2,3,4,5};
+		int array[] = {0,1,2,3,4,5};
 		
 		System.out.println(array[0]); //Puede ser 0, 1, 2, 3, 4...
 
